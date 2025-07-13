2d4be87ccb849ec9f40937a5cd11c380abd6589c
Kenshi <vklapshov@gmail.com>
2025-07-13 19:27:21 +0300

    change about page name

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -5,17 +5,15 @@ import { cn } from "@bem-react/classname";
 const bem = cn("About");
 
 export const About: React.FC = () => {
-  return (
-    <div className={bem()}>
-      <Helmet title="About" />
-      <div className="row">
-        <div className="col">
-          <h1>About</h1>
-          <p>
-            Author: Лапшов Валерий
-          </p>
+    return (
+        <div className={bem()}>
+            <Helmet title="About" />
+            <div className="row">
+                <div className="col">
+                    <h1>About</h1>
+                    <p>Author: Лапшов Валерий1</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
